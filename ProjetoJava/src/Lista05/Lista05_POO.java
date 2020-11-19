434a3bbfd92f37ef2f6477a479db8672677edc78
Ercília R S Dantas <72762687+ErciliaR@users.noreply.github.com>
2020-11-19 15:23:09 -0300

    Update Lista05_POO.java

--- ProjetoJava/src/Lista05/Lista05_POO.java
+++ ProjetoJava/src/Lista05/Lista05_POO.java
@@ -6,21 +6,18 @@ public class Lista05_POO
 	{
 		System.out.println("\nAUTOMÓVEL:");
 		System.out.println("O que é: um meio de trasporte movido a motor.");
-		System.out.println("Atributos: cor, modelo, placa, velocidade, marcha, tipo (e.g.: caminhão, ônibus, carro).");
+		System.out.println("Atributos: cor, modelo, placa, velocidade, marcha, tipo (e.g.: caminhão, ónibus, carro).");
 		System.out.println("Métodos: ligar/desligar, acelerar/freiar, mudar marcha, dar ré. ");
 		System.out.println();
 		System.out.println("\nPESSOA:");
-		System.out.println("O que é: uma denominação dada a um indivíduo da espécie humana.");
+		System.out.println("O que é: uma denominação dada a um individuo da espécie humana.");
 		System.out.println("Atributos: nome, idade, peso, altura, nivel de escolaridade, nacionalidade.");
 		System.out.println("Métodos: dormir/acordar, correr/andar, sentar/ficar em pé, comer/beber, estudar. ");
 		System.out.println();
 		System.out.println("\nCASA:");
-		System.out.println("O que �: tipo de moradia na qual uma pessoa ou mais pessoas habitam.");
-		System.out.println("Atributos: tamanho de terreno, cor, endere�o, taxa de IPTU, tipo (e.g: taipo, madeira, tijolos, concreto).");
-		System.out.println("M�todos: entrar/sair, limpar, pintar, reformar, adicionar m�veis, regularizar os documentos.");
 		System.out.println("O que é: tipo de moradia na qual uma pessoa ou mais pessoas habitam.");
 		System.out.println("Atributos: tamanho de terreno, cor, endereço, taxa de IPTU, tipo (e.g: taipo, madeira, tijolos, concreto).");
-		System.out.println("Métodos: dormir/acordar, correr/andar, sentar/ficar em pé, comer/beber, estudar.");
+		System.out.println("Métodos: entrar/sair, limpar, pintar, reformar, adicionar móveis, regularizar os documentos.");
 		System.out.println();
 		System.out.println("\nCOMO ESSES OBJETOS INTERAGEM ENTRE SI:");
 		System.out.println("Uma pessoa pode residir em uma casa e usar um automóvel para percorrer longas e curtas distâncias, podendo ");
