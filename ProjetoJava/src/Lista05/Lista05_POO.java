8c5b3c77a9305f6912165005df825d3907d36a1e
Ercília R S Dantas <72762687+ErciliaR@users.noreply.github.com>
2020-11-19 14:49:04 -0300

    Update Lista05_POO.java

--- ProjetoJava/src/Lista05/Lista05_POO.java
+++ ProjetoJava/src/Lista05/Lista05_POO.java
@@ -4,23 +4,23 @@ public class Lista05_POO
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("\nAUTOM”VEL:");
-		System.out.println("O que È: um meio de trasporte movido a motor.");
-		System.out.println("Atributos: cor, modelo, placa, velocidade, marcha, tipo (e.g.: caminh„o, Ùnibus, carro).");
-		System.out.println("MÈtodos: ligar/desligar, acelerar/freiar, mudar marcha, dar rÈ. ");
+		System.out.println("\nAUTOM√ìVEL:");
+		System.out.println("O que √©: um meio de trasporte movido a motor.");
+		System.out.println("Atributos: cor, modelo, placa, velocidade, marcha, tipo (e.g.: caminh√£o, √¥nibus, carro).");
+		System.out.println("M√©todos: ligar/desligar, acelerar/freiar, mudar marcha, dar r√©. ");
 		System.out.println();
 		System.out.println("\nPESSOA:");
-		System.out.println("O que È: uma denominaÁ„o dada a um indivÌduo da espÈcie humana.");
+		System.out.println("O que √©: uma denomina√ß√£o dada a um indiv√≠duo da esp√©cie humana.");
 		System.out.println("Atributos: nome, idade, peso, altura, nivel de escolaridade, nacionalidade.");
-		System.out.println("MÈtodos: dormir/acordar, correr/andar, sentar/ficar em pÈ, comer/beber, estudar. ");
+		System.out.println("M√©todos: dormir/acordar, correr/andar, sentar/ficar em p√©, comer/beber, estudar. ");
 		System.out.println();
-		System.out.println("\nCASA");
-		System.out.println("O que È: tipo de moradia na qual uma pessoa ou mais pessoas habitam.");
-		System.out.println("Atributos: tamanho de terreno, cor, endereÁo, taxa de IPTU, tipo (e.g: taipo, madeira, tijolos, concreto).");
-		System.out.println("MÈtodos: dormir/acordar, correr/andar, sentar/ficar em pÈ, comer/beber, estudar.");
+		System.out.println("\nCASA:");
+		System.out.println("O que √©: tipo de moradia na qual uma pessoa ou mais pessoas habitam.");
+		System.out.println("Atributos: tamanho de terreno, cor, endere√ßo, taxa de IPTU, tipo (e.g: taipo, madeira, tijolos, concreto).");
+		System.out.println("M√©todos: dormir/acordar, correr/andar, sentar/ficar em p√©, comer/beber, estudar.");
 		System.out.println();
 		System.out.println("\nCOMO ESSES OBJETOS INTERAGEM ENTRE SI:");
-		System.out.println("Uma pessoa pode residir em uma casa e usar um automÛvel para percorrer longas e curtas dist‚ncias, podendo ");
+		System.out.println("Uma pessoa pode residir em uma casa e usar um autom√≥vel para percorrer longas e curtas dist√¢ncias, podendo ");
 		System.out.println("ativamente agir de modo a mudar caracteristicas e/ou estados desses dois objetos.");
 	}
 }
