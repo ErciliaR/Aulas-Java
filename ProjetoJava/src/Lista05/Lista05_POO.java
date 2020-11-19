6934a9ef99dcd63330796eab6e3d17ce73b847ce
ErciliaR <72762687+ErciliaR@users.noreply.github.com>
2020-11-19 15:04:24 -0300

    POO ok

--- ProjetoJava/src/Lista05/Lista05_POO.java
+++ ProjetoJava/src/Lista05/Lista05_POO.java
@@ -14,10 +14,10 @@ public class Lista05_POO
 		System.out.println("Atributos: nome, idade, peso, altura, nivel de escolaridade, nacionalidade.");
 		System.out.println("Métodos: dormir/acordar, correr/andar, sentar/ficar em pé, comer/beber, estudar. ");
 		System.out.println();
-		System.out.println("\nCASA");
+		System.out.println("\nCASA:");
 		System.out.println("O que é: tipo de moradia na qual uma pessoa ou mais pessoas habitam.");
 		System.out.println("Atributos: tamanho de terreno, cor, endereço, taxa de IPTU, tipo (e.g: taipo, madeira, tijolos, concreto).");
-		System.out.println("Métodos: dormir/acordar, correr/andar, sentar/ficar em pé, comer/beber, estudar.");
+		System.out.println("Métodos: entrar/sair, limpar, pintar, furar parede, derrubar parede.");
 		System.out.println();
 		System.out.println("\nCOMO ESSES OBJETOS INTERAGEM ENTRE SI:");
 		System.out.println("Uma pessoa pode residir em uma casa e usar um automóvel para percorrer longas e curtas distâncias, podendo ");
